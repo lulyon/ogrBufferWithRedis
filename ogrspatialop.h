04cc0f60c055fa5835d8020a45320b8696b0879a
Lu Liang <lulyon@126.com>
2013-05-30 16:59:01 +0800

    Create ogrspatialop.h

---
+++ ogrspatialop.h
@@ -0,0 +1,12 @@
+/// @file ogrspatialop.h
+/// @author luliang@ict.ac.cn
+/// @copybrief Copyright 2012 ICT, CAS. All rights reserved.
+/// @version 0.6
+/// @date 2013-05-15
+
+#ifndef OGR_SPATIALOP_H_
+#define OGR_SPATIALOP_H_
+
+OGRLayer *ComputeBuffer(OGRLayer *poLayer, int radius_);
+
+#endif /* OGRSPATIALOP_H_ */
